7d3379f3ccfbe961bde0eb0bf0239ae90f67a13f
sam-abdulilah <sam.mohammed@soilla.com>
2026-04-29 13:15:41 +0300

    client-module&dto

--- src/modules/clients/clients.module.ts
+++ src/modules/clients/clients.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../infrastructure/prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [ClientsController],
   providers: [ClientsService],
+  exports: [ClientsService],
 })
 export class ClientsModule {}
